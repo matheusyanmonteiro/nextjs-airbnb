74ca268251c71985ae4455bd42be1a6cbb728e92
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2023-08-22 00:58:21 -0300

    feat: create footer and navbar

--- public/src/components/footer/Footer.jsx
+++ public/src/components/footer/Footer.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import Link from 'next/link';
+import { FiGlobe } from "react-icons/fi";
+import { PiCaretUpBold } from "react-icons/pi";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  const links = [
+    "privacy",
+    "terms",
+    "sitemap",
+    "company details",
+    "destinations",
+  ];
+
+  return (
+  <div className="px-20 border-t border-t-gray-200 py-4 flex justify-between w-full text-sm z-50 bg-white items-center">
+    <ul className="flex gap-3 font-normal">
+      <li>
+        &copy; {new Date().getFullYear()} Airbnb, Inc
+      </li>
+      {links.map((link) => (
+        <li key={link}>
+          <Link href="#" className="capitalize">
+            {/* {link.charAt(0).toUpperCase() + link.slice(1).toLowerCase()} */}
+            {link}
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <ul className="flex gap-4 font-medium">
+        <li className="flex items-center gap-2 cursor-pointer">
+          <FiGlobe /> English (IN)
+        </li>
+        <li className="cursor-pointer"> $ USD</li>
+        <li className="flex items-center gap-2 cursor-pointer">Support & resources <PiCaretUpBold/></li>
+    </ul>
+  </div>
+  ); 
 }
--- public/src/components/navbar/Navbar.jsx
+++ public/src/components/navbar/Navbar.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import AirBnbLogo from "../../svg/airbnb-logo";
+import Image from "next/image";
+import { FiGlobe } from "react-icons/fi";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full flex flex-col justify-center transition-all duration-300 h-20 border-b border-b-gray-200d">
+      <div className="flex items-center justify-between px-20">
+        <div className="flex-grow basis-0">
+          <div className="w-max cursor-pointer">
+            <AirBnbLogo />
+          </div>
+        </div>
+        <div className="flex-grow basis-0">
+          <ul className="flex items-center justify-end gap-6 font-medium">
+            <li className="cursor-pointer">
+              <span>Airbnb your home</span>
+            </li>
+            <li className="cursor-pointer">
+              <FiGlobe />
+            </li>
+            <li className="flex cursor-pointer items-center gap-2 border border-gray-300 py-2 px-3 rounded-full hover:shadow-xl transition-all duration-500">
+              <RxHamburgerMenu />
+              <span>
+                <Image src="/empty-profile.png" alt="profile" height={30} width={30} />
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div> 
+    </header>
+  )
 };
 
 export default Navbar;
